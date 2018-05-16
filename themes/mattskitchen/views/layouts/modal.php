67535227d3e14e9ed2f853c6a5d3e0ab9accdf13
olamide226 <olamideadebayo2001@gmail.com>
2018-05-16 13:34:46 +0100

    marketmoni 2.0 features: - added a knowledge base with responsive content - now compatible with all environments - major bug fixes - call logs are now shown on dta, aggregator and loan reconciliation

---
+++ themes/mattskitchen/views/layouts/modal.php
@@ -0,0 +1,321 @@
+<!-- Modal1 -->
+<div id="myModal1" class="modal fade" role="dialog">
+<div class="modal-dialog">
+
+<!-- Modal content-->
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal">&times;</button>
+<h4 class="modal-title">HOW TO REGISTER</h4>
+</div>
+<div class="modal-body">
+<p>VISIT: apply.marketmoni.com.ng<br>
+Step 1: Download the MOU template and fill as required.<br>
+Step 2: Download the excel sheet (fill all member details, three executive members are required)<br>
+Step 3: Fill the MOU <br>
+Step 4: Upload excel sheet and MOU<br>
+*** Use provided template for the excel sheet and scan the MOU and upload in PDF
+</p>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- Modal2 -->
+<div id="myModal2" class="modal fade" role="dialog">
+<div class="modal-dialog">
+
+<!-- Modal content-->
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal">&times;</button>
+<h4 class="modal-title"><b>LOAN RECONCILIATION</b></h4>
+</div>
+<div class="modal-body"><b>Loan & reconciliation sum breakdown:</b><br>
+<b>Amount:</b> Loan collected<br>
+<b>Amount due:</b> Loan collected + Interest<br>
+<b>Amount repaid:</b> Received so far by BOI <br>
+<b>Amount in default:</b> Expected repayment by customer <br>
+<b>Amount paid so far:</b>  Total sum paid by customer thus far (Requested by the Contact Centre agent) <br>
+<br>
+<b>Repayment:<br>
+***Insist customers make payment to their agent (the provided account number should only be provided in the case of fraud detection or other discretionary measure).</b><br>
+Account name:  BOI/GEEP<br>
+Bank: First Bank<br>
+Account number: <b>2032272651</b><br>
+<b>NOTE:</b> CUSTOMERS MUST WRITE THEIR PHONE NUMBER (REGISTERED WITH GEEP) ON THE TELLER (BESIDE THEIR NAME). They must also insist cashier input the provided number in their submission.<br>
+<br>
+<b>Non-disbursement of loan (after receipt of disbursement message):</b><br>
+We are sorry sir/ma,<br>
+We are rectifying the loan disbursement; your loan will be disbursed shortly. I will log in your complaint for speedy processing.<br>
+</p>
+<p><h5><strong>FORM/PERSONAL DETAILS CORRECTION</strong></h5>
+Please provide in order below (brief customer on the case sensitive letters)<br>
+BVN: https://goo.gl/7jn5iC<br>
+Extensive correction: https://goo.gl/yQLYD8
+</p></div>
+<div class="modal-footer">
+<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- Modal3 -->
+<div id="myModal3" class="modal fade" role="dialog">
+<div class="modal-dialog">
+
+<!-- Modal content-->
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal">&times;</button>
+<h4 class="modal-title"><b>LOAN OFFER SMS</b></h4>
+</div>
+<div class="modal-body">
+<p><b>Message comes with the following details:</b> Loan Amount, Interest/administrative fee, Tenure- 6 months.<br>
+<br><b>
+HOW TO RESPOND</b><br>
+<b>OPTION 1 </b>(SIMPLEST) ***If customer have message existing on their phone<br>
+Send BOIUniquecode to 32120 e.g. BOI2634 where unique code in received text message is 2634.<br>
+<br>
+<b>OPTION 2:</b> Short code: *540*265*unique number to 32120.<br>
+<b>OPTION 3</b> (WHEN MESSAGE HAS BEEN DELETED FROM CALLERS PHONE):<br>
+Dial *540*265#<br>
+Dropdown options are: <br>
+1. Update BVN<br>
+2. Check BOI loan offer<br>
+3. Reset<br>
+Choose 2 and proceed with the other guide.<br>
+</p>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
+</div>
+
+</div>
+</div>
+<!-- Modal4 -->
+<div id="myModal4" class="modal fade" role="dialog">
+<div class="modal-dialog">
+
+<!-- Modal content-->
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal">&times;</button>
+<h4 class="modal-title">LOAN PROCESSING ISSUE AGGREGATOR AND DTA</h4>
+</div>
+<div class="modal-body">
+<p>***Ask if customer/beneficiary applied via the marketmoni portal (DTA) or via form (Aggregator).<br>
+
+</p>
+  <div class="header"><h3>THE MAJOR DIFFERENCE BETWEEN THIS TWO ARE:</h3></div>
+
+   <table class='table table-striped table-bordered table-hover' cellspacing="0">
+      <tr>
+         <th><b>AGGREGATOR</b></th>
+         <th width='250'><b>DTA</b></th>
+      </tr>
+
+      <tr>
+        <td>Registered via form (hardcopy)
+        </td>
+        <td>Registered online (MarketMoni portal)
+        </td>
+      </tr>
+      <tr>
+        <td>Agent involved in registration</td>
+        <td>Agent not involved in registration
+        </td>
+      </tr>
+      <tr>
+        <td>Customers do not know their association (agents determines their association)</td>
+        <td>Customers know their association</td>
+      </tr>
+      <tr>
+        <td>Agent responsible for customer</td>
+        <td>Association leaders responsible for customer </td>
+      </tr>
+      <tr>
+        <td>Loan repayment is through Agents/Bank</td>
+        <td>Loan repayment is through Bank only</td>
+      </tr>
+    </table>
+
+    <p>***Check customer status on the portal, if status not existing in database, see response below<br>
+<b>RESPONSE:</b> We are processing your application, we process them based on ‘’first come first serve’’ basis.
+<br> We will get to your association in no time as we are receiving applications from the 36 States of the federation.</p>
+<p><b>APPLICATION STATUS DETAILS</b><br>
+<br>
+<b>Pending ICU</b> – 1st Step.<br>
+Customer has made it into BOI system. i.e. All information submitted is correct and application successful. At this point, customer receives loan offer SMS<br>
+<br>
+<b>Pending customer confirmation</b> - 2nd step. <br>
+Pending customer confirmation of Loan offer SMS. They have to reply with a unique code that was generated for them and contained in the text. Legal Aspect of loan process<br>
+<br>
+<b>Pending FI-Reconfirmation:</b> 3rd Step meaning Pending Financial inclusion reconfirmation.<br>
+After customer has responded to loan offer SMS. This is the approval from the directorate. <br>
+<br>
+<b>Pending Approval</b> – 4th Step.<br>
+Loan disbursed if approved. Loan is disbursed based on recommendation from the directorate (step 3 above)<br>
+Customer is credit worthy<br>
+Have satisfied all necessary conditions precedent to receiving GEEP MarketMoni loans.<br>
+Any customer with this status is on the Operations queue and due to be credited shortly.<br>
+</p>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- Modal5 -->
+<div id="myModal5" class="modal fade" role="dialog">
+<div class="modal-dialog">
+
+<!-- Modal content-->
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal">&times;</button>
+<h4 class="modal-title">FRAUD CASE</h4>
+</div>
+<div class="modal-body">
+<ul>
+  <li>Fraud is one of the categories of the inbound calls received by the call center agent. A case is suspected to be fraud base on the incidence reported by the customer.</li>
+<li>A suspected fraud case is meant to be escalated to the appropriate department in Bank of industry for further investigation.</li>
+<li>For Fraud cases, there are various types of such cases hence there are also details needed before such a case can be reported as fraud case.</li>
+<li>Some of the suspected fraud cases include;
+  <ol>
+    <li>Agent denying customers access to the loan.</li>
+  <li>Agent not giving full value of loan to the customer or not releasing the loan at all to the customer.</li>
+  <li>Agent trading with customer’s loan on behalf of the customer.</li>
+  <li>Agent not remitting customer loan repayment to Bank of Industry.</li>
+</ol>
+Therefore with such examples, it is necessary to get all facts concerning every cases to be reported.</li>
+<li>Some of the required information to be captured by the call center agent include:
+Customer’s name, Customer’s phone number, Association of the customer, Agent name, Agent phone number and Action (Action is the comment suggested by the call centers agent to be taking by Bank of Industry on the suspected cases).</li>
+</ul>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- Modal6 -->
+<div id="myModal6" class="modal fade" role="dialog">
+<div class="modal-dialog">
+
+<!-- Modal content-->
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal">&times;</button>
+<h4 class="modal-title">BOI INFORMATION</h4>
+</div>
+<div class="modal-body">
+  <p>
+  <b>BOI Customer Care:</b> 07002255264<br>
+  <b>Email:</b> customercare@boi.ng<br>
+  <b>NOTE:</b> for issues beyond GEEP/MARKETMONI mail: customercare@boi.ng<br>
+  <b>Upgrade me phone number:</b> 09031165613<br>
+  </p>
+<p><h5><b>WEB DETAILS</b></h5>
+Website: www.marketmoni.com.ng<br>
+E-mail: marketreg@boi.com.ng
+</p>
+
+<br><b>HOW TO APPLY AS AN AGENT/ SUB-AGGREGATOR</b>
+<ol>
+  <li>Visit www.marketmoni.com.ng/agent-network</li>
+  <li>Look for your proposed state of operation</li>
+  <li> Call the phone number(s) for guide</li>
+</ol>
+<h3>CHAIN OF LEVEL: </h3><p style="text-align:center;">
+                      BOI<br>
+					   <b>↓&darr;</b><br>
+		        		SUB AGGREGATORS<br>
+					   <b>↓&darr;</b><br>
+			        	       AGENTS<br>
+					   <b>↓&darr;</b><br>
+		            CUSTOMERS/BENEFICIARIES<br>
+</p>
+<h3>BOI ADDRESSES</h3>
+<table class="table table-hover table-striped table-bordered">
+  <thead>
+    <tr>
+      <th>S/N</th>
+      <th>BOI OFFICES</th>
+      <th>STATE COVERAGE</th>
+      <th>ADDRESS</th>
+      <th>CONTACT PERSON</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Head Office</td>
+      <td>Lagos</td>
+      <td>23, Marina street, Lagos.</td>
+      <td>Kemi Adekunle</td>
+    </tr>
+      <tr>
+      <td>2</td>
+      <td>Enugu Office</td>
+      <td>  Enugu and Ebonyi</td>
+      <td>47, coal city estate behind CBN building Enugu.</td>
+      <td>Gabriel Jonathan</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td> Kastina Office</td>
+      <td>Kastina</td>
+      <td>10, WTC road opp. CBN office, GRA. Kastina.</td>
+      <td>Mr Sadiq Ibrahim</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>Abuja Office</td>
+      <td>Abuja, Benue, Kogi and Nassarawa</td>
+      <td>Plot 256, zone A O, off Herbert Maculay way, behind unity bank, central business district, FCT – Abuja.</td>
+      <td>Chienyem Okorocha</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>Gombe Office</td>
+      <td>Adamawa, Gombe and Taraba</td>
+      <td>45, shongo housing estate, Gombe.</td>
+      <td>Aisha Adamu</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td>Bauchi Office</td>
+      <td>Bauchi, Borno, Plateau and Yobe</td>
+      <td>BOI house Maiduguri road, Bauchi.</td>
+      <td>Mr Sagir Ahmed</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>Osun Office</td>
+      <td>Osun</td>
+      <td>10, Obafemi Awolowo way igbonna, Osogbo.</td>
+      <td>Dr. Emmanuel Ojowuro</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
+</div>
+
+</div>
+</div>
